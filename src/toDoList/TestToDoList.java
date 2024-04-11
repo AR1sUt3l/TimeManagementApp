5c6ece463ec2a95fbdca2ef3af89c8b7efbfc989
AR1sUt3l <alecz08villagr@gmail.com>
2024-04-11 11:52:07 -0700

    Added TestToDoList class
    JUnit Test Case

---
+++ src/toDoList/TestToDoList.java
@@ -0,0 +1,16 @@
+package toDoList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestToDoList
+{
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
