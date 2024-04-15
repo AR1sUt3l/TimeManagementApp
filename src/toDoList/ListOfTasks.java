06ae2602b1dd6d0ecccd8dd45f13d7e2cceb9044
AR1sUt3l <alecz08villagr@gmail.com>
2024-04-14 23:25:25 -0700

    Update ListOfTasks.java
    added a method

--- src/toDoList/ListOfTasks.java
+++ src/toDoList/ListOfTasks.java
@@ -6,5 +6,36 @@ public class ListOfTasks implements SortByAlphabet, SortByDeadline, SortByType
 {
 	private LinkedList<Task> _list = new LinkedList<>();
 	
-	
+	public ListOfTasks(Task task)
+	{
+		
+	}
+
+	@Override
+	public Type[] getListOfTypes()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void sortByType()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sortByDeadline()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sortAlphabetically()
+	{
+		// TODO Auto-generated method stub
+		
+	}
 }
