d7228197485c8826e2633119dbb5b7ff847565d0
AR1sUt3l <alecz08villagr@gmail.com>
2024-04-14 21:39:58 -0700

    Update Task.java
    added variables
    added constructors

--- src/toDoList/Task.java
+++ src/toDoList/Task.java
@@ -1,6 +1,35 @@
 package toDoList;
 
+import java.sql.Time;
+
 public class Task
 {
-
+	private String _name;
+	private Type _taskType;
+	private Time _deadline;
+	
+	public Task(String name)
+	{
+		_name = name;
+		
+	}
+	
+	public Task(String name, Type taskType)
+	{
+		_name = name;
+		_taskType = taskType;
+	}
+	
+	public Task(String name, Time deadline)
+	{
+		_name = name;
+		_deadline = deadline;
+	}
+	
+	public Task(String name, Type taskType, Time deadline)
+	{
+		_name = name;
+		_taskType = taskType;
+		_deadline = deadline;
+	}
 }
