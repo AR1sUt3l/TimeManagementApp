11c1b8823a5f2d31ea639b6f68f6ce560ab593c7
AR1sUt3l <alecz08villagr@gmail.com>
2024-04-14 23:32:38 -0700

    Update TestToDoList.java
    added tests for getters and setters of Type class

--- src/toDoList/TestToDoList.java
+++ src/toDoList/TestToDoList.java
@@ -8,9 +8,14 @@ class TestToDoList
 {
 
 	@Test
-	void test()
+	void testType()
 	{
-		fail("Not yet implemented");
+		Type subject1 = new Type();
+		assertEquals("General", subject1.getType());
+		Type subject2 = new Type("English");
+		assertEquals("English", subject2.getType());
+		subject2.setType("Math 150");
+		assertEquals("Math 150", subject2.getType());
 	}
 
 }
