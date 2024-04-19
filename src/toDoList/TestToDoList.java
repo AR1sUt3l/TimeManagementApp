54fd6fa20565e82395efa50cad65002da685a80b
AR1sUt3l <alecz08villagr@gmail.com>
2024-04-18 23:27:41 -0700

    Update TestToDoList.java
    added new tests for type class

--- src/toDoList/TestToDoList.java
+++ src/toDoList/TestToDoList.java
@@ -10,12 +10,23 @@ class TestToDoList
 	@Test
 	void testType()
 	{
-		Type subject1 = new Type();
-		assertEquals("General", subject1.getType());
-		Type subject2 = new Type("English");
-		assertEquals("English", subject2.getType());
+		Type subject1 = new Type("english");
+		assertEquals("English", subject1.getType());
+		assertEquals("General, English", subject1.toString());
+		Type subject2 = new Type();
+		assertEquals("General", subject2.getType());
+		assertEquals("General, English", subject2.toString());
+		assertEquals("General, English", subject1.toString());
 		subject2.setType("Math 150");
+//		Type subject4 = new Type("Math 151");
 		assertEquals("Math 150", subject2.getType());
+		assertEquals("General, English, Math 150", subject2.toString());
+		assertEquals("General, English, Math 150", subject1.toString());
+		Type subject3 = new Type("black studies");
+		assertEquals("Black Studies", subject3.getType());
+		assertEquals("General, English, Math 150, Black Studies", subject3.toString());
+		subject3.sortAlphabetically();
+		assertEquals("General, Black Studies, English, Math 150", subject3.toString());
 	}
 	
 	@Test
