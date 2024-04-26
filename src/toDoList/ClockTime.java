410e2ed946183f402dd057b51a91c6a6e76407f9
AR1sUt3l <alecz08villagr@gmail.com>
2024-04-25 23:44:24 -0700

    Update ClockTime.java
    added temporary default constructor

--- src/toDoList/ClockTime.java
+++ src/toDoList/ClockTime.java
@@ -2,12 +2,14 @@ package toDoList;
 
 public class ClockTime extends CalendarDate
 {
+	public ClockTime(int date, int month, int year)
+	{
+		super(date, month, year);
+		// TODO Auto-generated constructor stub
+	}
 	private int _hours;
 	private int _minutes;
 	private int _seconds;
 	
-	public ClockTime()
-	{
-		
-	}
+	
 }
