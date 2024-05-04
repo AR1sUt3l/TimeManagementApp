61cbeeed68ad091f833b0040bf6cb8a42b6b0597
AR1sUt3l <alecz08villagr@gmail.com>
2024-05-03 23:40:48 -0700

    Update ToDoListView.java
    added code for default view of to do list

--- src/toDoList/ToDoListView.java
+++ src/toDoList/ToDoListView.java
@@ -1,6 +1,45 @@
 package toDoList;
 
-public class ToDoListView
-{
+import java.awt.FlowLayout;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class ToDoListView extends JFrame
+{
+	private static final String TITLE = "To Do List";
+	private static final String TEXT_FOR_TODAY = "Today";
+	
+	private JPanel _panel = new JPanel();
+	private JLabel _topText;
+	
+	public ToDoListView()
+	{
+		initialFrame();
+		initialUI();
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack();
+		setVisible(true);
+	}
+	
+	private void initialFrame()
+	{
+		setTitle(TITLE);
+	}
+	
+	private void initialUI()
+	{
+		setLayout(new FlowLayout());
+		addTopText();
+	}
+	
+	private void addTopText()
+	{
+		_topText = new JLabel(TEXT_FOR_TODAY);
+		_panel.add(_topText);
+	}
+	
 }
