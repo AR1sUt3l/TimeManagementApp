5a2b5469cd4252d8d55cb2aaa604f68c3c128a76
AR1sUt3l <alecz08villagr@gmail.com>
2024-05-03 23:18:45 -0700

    Update DueDate.java
    Test code

--- src/toDoList/DueDate.java
+++ src/toDoList/DueDate.java
@@ -5,11 +5,17 @@ import java.time.format.DateTimeFormatter;
 
 public class DueDate
 {
+	private DateTimeFormatter monthDayYear = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+//	private DateTimeFormatter monthDayYear = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 	private LocalDateTime _currentDate = LocalDateTime.now();
-	private DateTimeFormatter monthDayYear = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 	
 	public DueDate()
 	{
-		System.out.println(monthDayYear.format(_currentDate));
+		// System.out.println(monthDayYear.format(_currentDate));
+	}
+	
+	public String toString()
+	{
+		return monthDayYear.format(_currentDate);
 	}
 }
