6e01c81ad6275ac61e9600fe2d22f70055831802
AR1sUt3l <alecz08villagr@gmail.com>
2024-05-04 23:28:01 -0700

    Update ToDoListView.java
    add constant variables and method for size of window
    tried aligning text to the left

--- src/toDoList/ToDoListView.java
+++ src/toDoList/ToDoListView.java
@@ -9,11 +9,16 @@ import javax.swing.JTextField;
 
 public class ToDoListView extends JFrame
 {
+	private static final int WINDOW_WIDTH = 350;
+	private static final int WINDOW_HEIGHT = 500;
 	private static final String TITLE = "To Do List";
 	private static final String TEXT_FOR_TODAY = "Today";
+//	private static final String SEPARATION_LINE
 	
-	private JPanel _panel = new JPanel();
+	private JPanel _topPanel = new JPanel();
+	
 	private JLabel _topText;
+	private JLabel _lineBreak;
 	
 	public ToDoListView()
 	{
@@ -28,6 +33,7 @@ public class ToDoListView extends JFrame
 	private void initialFrame()
 	{
 		setTitle(TITLE);
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 	}
 	
 	private void initialUI()
@@ -39,8 +45,9 @@ public class ToDoListView extends JFrame
 	private void addTopText()
 	{
 		_topText = new JLabel(TEXT_FOR_TODAY);
-		_panel.add(_topText);
-		add(_panel);
+		_topPanel.add(_topText);
+		_topPanel.setAlignmentX(LEFT_ALIGNMENT);
+		add(_topPanel);
 	}
 	
 	public static void main(String[] args)
