eaa2eccbece7b5554a523e7271fd2712e5970d83
AR1sUt3l <alecz08villagr@gmail.com>
2024-05-11 22:20:52 -0700

    Update ListOfTypes.java
    added more methods to get list from a text file and vice versa
    completed code for reading from file to list

--- src/toDoList/ListOfTypes.java
+++ src/toDoList/ListOfTypes.java
@@ -1,11 +1,42 @@
 package toDoList;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class ListOfTypes implements SortByAlphabet
 {
+	private static final String DEFAULT_TYPE = "General";
 	private static ArrayList<String> _list = new ArrayList<>();
+	
+	public void updateTextFile() throws IOException
+	{
+		
+	}
+	
+	public void updateList() throws IOException
+	{
+		File listFile = new File("textFiles/TypesList.txt");
+		if (!listFile.exists())
+		{
+			System.out.println("Error: File Not Found");
+			System.exit(0);
+		}
+		Scanner readFile = new Scanner(listFile);
+		while (readFile.hasNext())
+		{
+			_list.add(readFile.nextLine());
+		}
+		readFile.close();
+	}
+	
+	public void addTask(Task task)
+	{
+		
+	}
 	
 	public ArrayList<String> getList()
 	{
