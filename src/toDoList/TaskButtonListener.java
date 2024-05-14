2009ad9b9729f3fd51b0fddeff12b108ab8b927d
AR1sUt3l <alecz08villagr@gmail.com>
2024-05-13 23:58:44 -0700

    Create TaskButtonListener.java
    created taskbuttonlistener class for action listener for 'add task' button

---
+++ src/toDoList/TaskButtonListener.java
@@ -0,0 +1,15 @@
+package toDoList;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TaskButtonListener implements ActionListener
+{
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+//		Task _task = new Task(e.)
+		
+	}
+
+}
