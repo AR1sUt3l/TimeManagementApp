ca6dda197a8817390a5bef34bfd87f15d31b96d1
AR1sUt3l <alecz08villagr@gmail.com>
2024-05-16 23:57:10 -0700

    Create ToDoListController.java
    includes TaskButtonListener removed earlier

---
+++ ToDoList/toDoList/ToDoListController.java
@@ -0,0 +1,18 @@
+package toDoList;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ToDoListController
+{
+	private class TaskButtonListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e)
+		{
+	//		Task _task = new Task(e.)
+			
+		}
+	}
+
+}
