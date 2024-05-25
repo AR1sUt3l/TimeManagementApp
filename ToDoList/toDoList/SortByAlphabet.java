bf386168b342d745e967d15d08979bdb83eea357
AR1sUt3l <alecz08villagr@gmail.com>
2024-05-24 23:40:52 -0700

    Update SortByAlphabet.java

--- ToDoList/toDoList/SortByAlphabet.java
+++ ToDoList/toDoList/SortByAlphabet.java
@@ -1,5 +1,25 @@
 package toDoList;
 
+/**
+ * Lead Author(s):
+ * @author Aleczandria Villagracia
+ * <<add additional lead authors here, with a full first and last name>>
+ * 
+ * Other contributors:
+ * <<add additional contributors (mentors, tutors, friends) here, with contact information>>
+ * 
+ * References:
+ * Morelli, R., & Walde, R. (2016). Java, Java, Java: Object-Oriented Problem Solving.
+ * Retrieved from https://open.umn.edu/opentextbooks/textbooks/java-java-java-object-oriented-problem-solving
+ * 
+ * <<add more references here>>
+ *  
+ * Version/date: 05.24.2024.01
+ * 
+ * Responsibilities of class:
+ * 
+ */
+
 public interface SortByAlphabet
 {
 	public void sortAlphabetically();
