9b7da47e9e402bb6ede31aed5fd819a481014936
AR1sUt3l <alecz08villagr@gmail.com>
2024-05-24 23:40:20 -0700

    Update DueDate.java
    changes due date to be a string

--- ToDoList/toDoList/DueDate.java
+++ ToDoList/toDoList/DueDate.java
@@ -4,46 +4,77 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Date;
+
+/**
+ * Lead Author(s):
+ * @author Aleczandria Villagracia
+ * <<add additional lead authors here, with a full first and last name>>
+ * 
+ * Other contributors:
+ * <<add additional contributors (mentors, tutors, friends) here, with contact information>>
+ * 
+ * References:
+ * Morelli, R., & Walde, R. (2016). Java, Java, Java: Object-Oriented Problem Solving.
+ * Retrieved from https://open.umn.edu/opentextbooks/textbooks/java-java-java-object-oriented-problem-solving
+ * 
+ * <<add more references here>>
+ *  
+ * Version/date: 05.24.2024.01
+ * 
+ * Responsibilities of class:
+ * 
+ */
 
 public class DueDate
 {
 	private DateTimeFormatter monthDayYear = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 	private LocalDateTime currentDate = LocalDateTime.now();
-	private Date dueDate;
+	private String dueDate;
 	private SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
 	private String noDueDate = "";
 	
 	public DueDate(String date)
 	{
-		if (date == "")
-		{
-			noDueDate = "No Deadline";
-		}
-		else
-		{
-			try
-			{
-				dueDate = sdf.parse(date);
-			}
-			catch (ParseException e)
-			{
-				System.out.println("Incorrect Date Format");
-			}
-		}
+//		if (date == "" || date == null)
+//		{
+//			noDueDate = "No Deadline";
+//		}
+//		else
+//		{
+//			try
+//			{
+//				dueDate = sdf.parse(date);
+//			}
+//			catch (IllegalArgumentException e)
+//			{
+//				System.out.println("Invalid Date");
+//			}
+//			catch (ParseException e)
+//			{
+//				System.out.println("Incorrect Date Format");
+//			}
+//		}
+		this.dueDate = date;
 	}
 	
+	/**
+	 * 
+	 * @return today's date as a string
+	 */
+	public String getCurrentDate()
+	{
+		return monthDayYear.format(currentDate);
+	}
+	
+	/**
+	 * @return the due date
+	 */
 	public String toString()
 	{
-		if (noDueDate == "")
-		{
-			return noDueDate;
-		}
-		else
-		{
-			return sdf.format(dueDate);
-		}
+		return dueDate;
 	}
 }
