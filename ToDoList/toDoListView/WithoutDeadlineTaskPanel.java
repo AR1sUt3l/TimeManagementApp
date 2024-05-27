b48c724ebcfd65edbcaa09d07d611ea0c86565f9
AR1sUt3l <alecz08villagr@gmail.com>
2024-05-27 09:08:02 -0700

    Create WithoutDeadlineTaskPanel.java
    put code into the set labels

---
+++ ToDoList/toDoListView/WithoutDeadlineTaskPanel.java
@@ -0,0 +1,27 @@
+package toDoListView;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
+
+import toDoList.Task;
+
+public class WithoutDeadlineTaskPanel extends TaskPanel
+{
+
+	public WithoutDeadlineTaskPanel(Task task, ToDoListView mainView)
+	{
+		super(task, mainView);
+	}
+
+	@Override
+	public void setLabels()
+	{
+		textPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		nameLabel = new JLabel(task.getName());
+		textPanel.add(nameLabel);
+		textPanel.add(deadline);
+	}
+
+}
