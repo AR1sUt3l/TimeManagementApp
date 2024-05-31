7f61a064c6573c6fc5352a7e598edc0cf35221bd
AR1sUt3l <alecz08villagr@gmail.com>
2024-05-30 22:33:59 -0700

    Update WithDeadlinePanel.java
    working to add a scroll bar when task panels exceed size of panel

--- ToDoList/toDoListView/WithDeadlinePanel.java
+++ ToDoList/toDoListView/WithDeadlinePanel.java
@@ -1,5 +1,6 @@
 package toDoListView;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
@@ -9,6 +10,7 @@ import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import toDoList.DueDate;
 import toDoList.ListOfTasks;
@@ -17,9 +19,12 @@ import toDoList.Task;
 public class WithDeadlinePanel extends JPanel
 {
 	private JLabel dueDate;
+	private JScrollPane scrollPane;
 	
 	private Task task;
 	private ToDoListView mainView;
+	
+	private int count = 0;
 	
 	public WithDeadlinePanel(ToDoListView mainView)
 	{
@@ -29,11 +34,20 @@ public class WithDeadlinePanel extends JPanel
 		setBorder(BorderFactory.createLineBorder(Color.PINK));
 		setBackground(Color.WHITE);
 		setOpaque(false);
+//		setVisibleRowCount(3);
+//		JScrollPane scrollPane = new JScrollPane(nameList);
 	}
 	
 	public void addTaskPanel(Task newTask)
 	{
 		add(new WithDeadlineTaskPanel(newTask, mainView));
+		count++;
+		if (count == 6)
+		{
+			scrollPane = new JScrollPane(this);
+			add(scrollPane);
+		}
+		revalidate();
 	}
 
 }
